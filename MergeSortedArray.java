35f5f1ced11e32b978b78d870dc6e2b99d9a72b8
Anushka Singh <78140910+Nushcode@users.noreply.github.com>
2022-07-05 19:25:24 +0530

    Add files via upload

---
+++ MergeSortedArray.java
@@ -0,0 +1,21 @@
+package Day1;
+
+public class MergeSortedArray {
+    class Solution {
+        public void merge(int[] nums1, int m, int[] nums2, int n) {
+            int a = m - 1;
+            int b = n - 1;
+            for(int i=m+n-1;i>=0;i--){
+                if(a<0){
+                    nums1[i] = nums2[b--];
+                }else if(b<0){
+                    break;
+                }else if(nums1[a]>nums2[b]){
+                    nums1[i] = nums1[a--];
+                }else{
+                    nums1[i]=nums2[b--];
+                }
+            }
+        }
+    }
+}
